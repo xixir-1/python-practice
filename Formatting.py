8c821d4effd14b196b669389e4cc12eb40e7c903
xixir-1 <jmshishir2@gmail.com>
2026-09-24 19:24:39 +0600

    Add files via upload

---
+++ Formatting.py
@@ -0,0 +1,14 @@
+n=45
+n2=67
+print ('this is my super namber',n+n2 )
+
+
+num1=56
+num2=67
+print(f'this is my super number {num1+num2}' )
+# 1st f tarpor {}
+
+name='Mahim'
+fvt='haking'
+dream='jannat'
+print(f'My name is {name} , My Dream is {dream} and My hobi is {fvt} ')
---
+++ binary type bytes and bytearray.py
@@ -0,0 +1,12 @@
+# binary type data
+marklist=[33,55,64,89,76,78,78,]
+m=bytes(marklist)
+print(type(m))
+
+
+# binary type data 
+
+marklist=[67,78,90,34,56,]
+m1=bytearray (marklist)
+# m1[0]=45
+print(m1[0])
