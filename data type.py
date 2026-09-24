fd3a8c07828ae3c2436fbd74ca4489baf12d1a57
xixir-1 <jmshishir2@gmail.com>
2026-09-24 15:23:12 +0600

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,39 @@
+#  str type data
+xixir3='mahim'
+xixir4='356'
+print(type(xixir3))
+print(type(xixir4))
+print ( 'my name is ' + xixir3 + ' ' + xixir4)
+
+
+
+# int type data
+xixir =230
+print (xixir)
+print (type(xixir))
+
+
+#  floating type data
+xixir1 =240.4
+print(xixir1)
+print (type(xixir1))
+
+
+#  complex type data
+#  sudhu j deoya lagbe complex ar jono
+xixir2 =340j
+print(xixir2)
+print(type(xixir2))
+
+
+# bool type data
+boool=True
+phon=False
+print (boool)
+print(type(boool))
+
+
+
+
+
+
