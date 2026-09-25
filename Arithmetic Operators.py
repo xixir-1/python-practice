293c32a16e65b88d727cda0692955c1557cbfbff
xixir-1 <jmshishir2@gmail.com>
2026-09-25 16:38:51 +0600

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,24 @@
+
+x=7
+y=4
+
+# Addition
+print(x+y)
+
+# Subtraction
+print(x-y)
+
+# Multiplication
+print(x*y)
+
+# Division
+print(x/y)
+
+# Modulus
+print(x%y)
+
+# Exponentiation
+print(x**y)
+
+# Floor division
+print(x//y)
---
+++ sequence type data.py
@@ -0,0 +1,18 @@
+# list type data
+li =['mahim', 'xixir', 'siam']
+li[2]= 'sifat'
+print (li)
+print(type(li))
+
+# tuple type data
+lis =('mahim','xixir', 'siam')
+# lis[2]='sifat'
+print(lis)
+print(type(lis))
+
+# range type data
+ran=range(4);
+print(ran)
+print(type(ran))
+for i in ran :
+    print(i)
