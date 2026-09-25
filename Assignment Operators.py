1723393554f82b125ee885640fddef541aeb33e6
xixir-1 <jmshishir2@gmail.com>
2026-09-25 19:06:16 +0600

    Add files via upload

---
+++ Assignment Operators.py
@@ -0,0 +1,33 @@
+m=6
+
+# +=
+m +=7
+print(m)
+
+# -=
+m-=2
+print(m)
+
+# 8=
+m*=5
+print(m)
+
+# /=
+m/=3
+print(m)
+
+n=8
+#  %=
+n%=3
+print(n)
+
+# //=
+b=9
+b//=2
+print(b)
+
+# **=
+v=4
+v**=2
+print(v)
+
---
+++ input.py
@@ -0,0 +1,5 @@
+username=input('enter your name:')
+password=input('enter your password :')
+
+print(username)
+print(password)
